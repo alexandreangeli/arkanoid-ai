ed267e2404801f583c13a41faa2e93fac2cf74e9
ale.angeli@hotmail.com <ale.angeli@hotmail.com>
2020-06-07 01:09:01 -0300

    rowcount

--- brick.js
+++ brick.js
@@ -6,7 +6,7 @@ class Brick {
     this.height = height;
     this.destroyed = destroyed;
     this.life =
-      Math.floor(Math.random() * Math.floor(ballShooter.maxBalls / 3)) + 1;
+      Math.floor(Math.random() * Math.floor(ballShooter.maxBalls / 2)) + 1;
     this.color = "#0095DD";
   }
 }
