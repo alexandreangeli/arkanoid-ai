09d6e2198128e9698082b10119a99e1b0ce8a87c
ale.angeli@hotmail.com <ale.angeli@hotmail.com>
2020-06-07 20:41:55 -0300

    changed bricks lifes

--- brick.js
+++ brick.js
@@ -5,9 +5,18 @@ class Brick {
     this.width = width;
     this.height = height;
     this.destroyed = Math.random() > 0.6;
-    this.life =
-      Math.floor(Math.random() * Math.floor(ballShooter.maxBalls / 2)) +
-      (Math.floor(ballShooter.maxBalls * 0.5) || 1);
+
+    random = Math.random() * 100 + 0;
+    if (random < 25) {
+      this.life = Math.floor(ballShooter.maxBalls / 2);
+    }
+    if (random >= 25 && random < 75) {
+      this.life = ballShooter.maxBalls;
+    }
+    if (random >= 75) {
+      this.life = ballShooter.maxBalls * 2;
+    }
+
     this.color = "#0095DD";
   }
 }
