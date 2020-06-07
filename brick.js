7e81d06f9d0f7fd55346f22d474f4a796cf25901
ale.angeli@hotmail.com <ale.angeli@hotmail.com>
2020-06-07 20:31:16 -0300

    bricks starting destroyed

--- brick.js
+++ brick.js
@@ -1,10 +1,10 @@
 class Brick {
-  constructor(x, y, width, height, destroyed, color) {
+  constructor(x, y, width, height) {
     this.x = x;
     this.y = y;
     this.width = width;
     this.height = height;
-    this.destroyed = destroyed;
+    this.destroyed = Math.random() > 0.6;
     this.life =
       Math.floor(Math.random() * Math.floor(ballShooter.maxBalls / 2)) +
       (Math.floor(ballShooter.maxBalls * 0.5) || 1);
